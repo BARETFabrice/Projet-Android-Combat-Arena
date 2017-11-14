787f86a3cbd5fa86e81e36ed7a7d6bbc6e354412
BARETFabrice <baretfabrice974@gmail.com>
2017-11-14 17:25:47 -0500

    initial commit android studio

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/combatarena/VueCombatArena.java
@@ -0,0 +1,15 @@
+package ca.qc.cgmatane.informatique.combatarena;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueCombatArena extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_combat_arena);
+    }
+}
