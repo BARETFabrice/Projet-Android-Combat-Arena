85cc7803d2689b70ae4958e6d5439c9084abe271
fsimoneau <fsimoneau997@gmail.com>
2017-11-21 16:16:59 -0500

    Début inscription + librairie md5

--- app/src/main/assets/www/action/vue/InscriptionVue.js
+++ app/src/main/assets/www/action/vue/InscriptionVue.js
@@ -3,6 +3,17 @@ const InscriptionVue = function()
     function initialiser()
     {
         document.querySelector("body").innerHTML = Page.pageInscription;
+		document.getElementById("formulaire-inscription").addEventListener("submit", function(evenement){
+			alert("Inscription faite!");
+			evenement.preventDefault();
+			//Encrypter mot de passe
+			//Envoyer au service web
+			//	si c'est bon --> Localstore updateCommands
+			//	si ce n'est pas bon --> retour à la page inscription avec alert
+			window.location.hash = "#";
+		});
     }
     initialiser();
 };
+
+
