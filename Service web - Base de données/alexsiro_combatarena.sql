7ce57836cb2d24170faad865373487d88fc519dd
fsimoneau <fsimoneau997@gmail.com>
2017-12-14 09:46:24 -0500

    Mise à jour de la base de données avec des utilisateurs et des parties jouees

--- Service web - Base de données/alexsiro_combatarena.sql
+++ Service web - Base de données/alexsiro_combatarena.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 21, 2017 at 02:54 PM
+-- Generation Time: Dec 14, 2017 at 09:45 AM
 -- Server version: 10.1.24-MariaDB-cll-lve
 -- PHP Version: 5.6.30
 
@@ -30,23 +30,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Partie` (
   `id` int(11) NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Partie-Utilisateur`
---
-
-CREATE TABLE `Partie-Utilisateur` (
-  `id` int(11) NOT NULL,
-  `idPartie` int(11) NOT NULL,
   `idUtilisateur` int(11) NOT NULL,
-  `frag` int(11) NOT NULL,
-  `mort` int(11) NOT NULL,
-  `position_classement` int(11) NOT NULL
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `victoire` tinyint(1) NOT NULL,
+  `nombreDeChutes` int(11) NOT NULL,
+  `dureePartie` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Partie`
+--
+
+INSERT INTO `Partie` (`id`, `idUtilisateur`, `date`, `victoire`, `nombreDeChutes`, `dureePartie`) VALUES
+(1, 1, '2017-12-13 19:34:27', 1, 2, 60183),
+(2, 2, '2017-12-13 19:34:55', 0, 5, 41847),
+(3, 2, '2017-12-13 19:35:16', 1, 1, 56039),
+(4, 3, '2017-12-13 19:35:38', 1, 1, 45453),
+(5, 1, '2017-12-13 19:35:54', 1, 0, 64058);
 
 -- --------------------------------------------------------
 
@@ -62,8 +62,24 @@ CREATE TABLE `Utilisateur` (
   `age` int(11) NOT NULL,
   `couleur_logo` varchar(64) NOT NULL,
   `volumeSon` int(11) NOT NULL,
-  `date_de_creation` datetime NOT NULL
+  `date_de_creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `token` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`id`, `login`, `mot_de_passe`, `courriel`, `age`, `couleur_logo`, `volumeSon`, `date_de_creation`, `token`) VALUES
+(1, 'admin', '$2y$10$TPj5r63m0sLlJiKSAroKcuacuxPs/bNxc3pFIF5brk9taw6dnwGRC', 'test@gmail.com', 42, '#000000', 0, '2017-12-13 18:59:47', 'RblLS0h5lJMxHHydswb0'),
+(2, 'fred', '$2y$10$.H5MFYOKIAOJNJb7W.QtbeoTkOIt9aat5cibBBpqkTRLN1FaeRXF6', 'fred@fred.com', 1, '#400080', 100, '2017-12-13 19:01:22', NULL),
+(3, 'alex', '$2y$10$xW5XP3N8yYc.6DWKpIp1C.EL.WxK75bSfmypLga6wns82FNwftoX6', 'alex@alex.alex', 125, '#00ff00', 75, '2017-12-13 19:04:24', NULL),
+(4, 'fabrice', '$2y$10$TvwBKCCYoziJAl6nF8ETx.lkqXMXy2xK7EZM9Ij0EZjLlPcPyLyoq', 't@t.c', 1, '#000000', 0, '2017-12-13 20:25:46', 'kZpVJhKSPcrrLYB6gtzW'),
+(5, 'test1', '$2y$10$mfqed/Y5.h4wYGBfqfK6peHsFkeErmAcLibyln4cqePKKpIHimt/a', 'test@test.com', 12, '#c0c0c0', 100, '2017-12-13 20:26:58', NULL),
+(6, 'test2', '$2y$10$xFItcmlBxvMuWXnRRDsQceO7e4Sn5J5Van3JpyMyoYviy.SPzXMuy', 'test2@tr.cp', 2, '#808080', 54, '2017-12-13 20:28:16', NULL),
+(7, 'test4', '$2y$10$EGP4tJIxzPXylw.GLl595uM5fQFO6nfO9wmLfa5ZMXc1M7PetC/a.', 'test@4.4444', 44, '#408080', 50, '2017-12-13 20:33:39', NULL),
+(8, 'test3', '$2y$10$tu0dSUz8G6556KbJqkRnyucQrDxWace/FsIK6kQ8/RmwWqvA.9i.O', 'test@test.test', 3, '#00ff00', 0, '2017-12-13 20:35:35', 'Y5lTleJzeHE79tQGMAQH'),
+(9, 'test42', '$2y$10$FO13tKIlnuuQ7ssfMTkx2.w14qcScmitqMmdIb6CVgfX00PIUWP92', 't@t.c', 42, '#004040', 8, '2017-12-13 23:04:49', NULL);
 
 --
 -- Indexes for dumped tables
@@ -73,12 +89,6 @@ CREATE TABLE `Utilisateur` (
 -- Indexes for table `Partie`
 --
 ALTER TABLE `Partie`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `Partie-Utilisateur`
---
-ALTER TABLE `Partie-Utilisateur`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -95,17 +105,12 @@ ALTER TABLE `Utilisateur`
 -- AUTO_INCREMENT for table `Partie`
 --
 ALTER TABLE `Partie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `Partie-Utilisateur`
---
-ALTER TABLE `Partie-Utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `Utilisateur`
 --
 ALTER TABLE `Utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
