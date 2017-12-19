24a1035e2869162ed40f18a0beecded52e0bb632
fsimoneau <fsimoneau997@gmail.com>
2017-12-19 18:32:23 -0500

    Ajout service web Moyenne des statistiques des joueurs

---
+++ Service web - Base de données/moyenneStatsJoueur.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'alexsiro_combat_arena_admin');
+define('DB_PASSWORD', 'HZM]jeyhyHw9');
+define('DB_NAME', 'alexsiro_combatarena');
+ 
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+$id = (int) $_GET['id'];
+ 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "SELECT AVG(nombreDeChutes) as chutes, AVG(dureePartie) as duree, SUM(victoire) / (COUNT(*) * 1.0) as ratio FROM Partie WHERE idUtilisateur = $id";
+$result = $link->query($sql);
+$myArray = array();
+$i = 0;
+
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$myArray[$i] = $row;
+		$i++;
+	}
+	$json = json_encode($myArray[0]);
+	echo $json;
+} else {
+	echo '{"error":true}';
+}
+?>
